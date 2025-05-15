ba0875205cc389a8b05d6ba9cf5b3c610bd34a02
01091081878 <110251713+01091081878@users.noreply.github.com>
2025-05-15 10:51:57 +0300

    regex_to_dfa.py

--- regex_to_dfa.py
+++ regex_to_dfa.py
@@ -1,14 +1,17 @@
-import re
+from automata.fa.dfa import DFA
+from automata.fa.nfa import NFA
+from automata.regex.regex import Regex
 
-class DFA:
-    def __init__(self, pattern):
-        self.pattern = re.compile(f"^{pattern}$")
-
-    def accepts(self, string):
-        return bool(self.pattern.match(string))
-
-def regex_to_dfa(regex: str) -> DFA:
-    return DFA(regex)
-
-assert regex_to_dfa("(a|b)*abb").accepts("aabb") == True
-assert regex_to_dfa("(a|b)*abb").accepts("ababa") == False
+class RegexToDFA:
+    def __init__(self, regex_str: str):
+        self.regex_str = regex_str
+        self.dfa = self.regex_to_dfa(regex_str)
+        
+    def regex_to_dfa(self, regex_str: str) -> DFA:
+        regex_obj = Regex(regex_str)
+        nfa = regex_obj.to_epsilon_nfa()
+        dfa = DFA.from_nfa(nfa)
+        return dfa
+    
+    def accepts(self, string: str) -> bool:
+        return self.dfa.accepts_input(string)
